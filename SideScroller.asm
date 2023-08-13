089cfdcf73f9cf1f72960685bb24c4e30e95b5ee
CoffeeCatRailway <dragonlee1002@gmail.com>
2023-08-13 13:35:57 +1000

    Basic macro to place tile

--- SideScroller.asm
+++ SideScroller.asm
@@ -16,6 +16,19 @@
 	.ENDR
 .ENDM
 
+; 0400 = 0000 0100 0000 0000
+; E.g. SetTile $0400, $01
+;-------------------------
+; In: POSITION - Flipped, palette & position
+;     TILE_ID - What tile to use
+;-------------------------
+.MACRO SetTile
+	ldx #\1		; High: vhopppcc Low: cccccccc
+	stx $2116	; c: Starting character (tile) number
+	lda #\2		; h: horizontal flip	v: vertical flip
+	sta $2118	; p: palette number	o: priority bit
+.ENDM
+
 .BANK 0 SLOT 0
 .ORG 0
 .SECTION "MainCode"
@@ -29,10 +42,10 @@ Start:
 	stz PalNum
 	
 	; Load palette for tiles
-	LoadPalette BG_Palette, 0, 28		; Palette: BG_Palette, Start Color: 0, Size: 4 (number of colors NOT BPP)
+	LoadPalette BG_Palette, 0, 28		; Palette: BG_Palette, Start Color: 0, Size: 28 (number of colors NOT BPP)
 
 	; Load tile data to VRAM
-	LoadBlockToVRAM Tiles, $0000, $0040	; 2 tiles, 2bpp = 32 bytes
+	LoadBlockToVRAM Tiles, $0000, $0050	; 1 tile = 16 bytes (0010)
 
 	; Now, load up some data into our tilemap
 	; (If you had a full map, you could use LoadBlockToVRAM)
@@ -40,13 +53,78 @@ Start:
 	lda #$80
 	sta $2115
 	
-	; ==Display tile #1 in top left==
-	ldx #$0400
-	stx $2116
+	;ldx #$0400
+	;stx $2116
+	;lda #$01
+	;sta $2118
 	
-	lda #$00
-	sta $2118
-	; ===============================
+	SetTile $0400, $01
+	SetTile $0401, $01
+	SetTile $0402, $01
+	SetTile $0403, $01
+	SetTile $0404, $01
+	SetTile $0405, $01
+	SetTile $0406, $01
+	SetTile $0407, $01
+	SetTile $0408, $01
+	SetTile $0409, $01
+	SetTile $040A, $01
+	SetTile $040B, $01
+	SetTile $040C, $01
+	SetTile $040D, $01
+	SetTile $040E, $01
+	SetTile $040F, $01
+	
+	SetTile $0410, $01
+	SetTile $0411, $01
+	SetTile $0412, $01
+	SetTile $0413, $01
+	SetTile $0414, $01
+	SetTile $0415, $01
+	SetTile $0416, $01
+	SetTile $0417, $01
+	SetTile $0418, $01
+	SetTile $0419, $01
+	SetTile $041A, $01
+	SetTile $041B, $01
+	SetTile $041C, $01
+	SetTile $041D, $01
+	SetTile $041E, $01
+	SetTile $041F, $01
+	
+	SetTile $0420, $01
+	SetTile $0421, $01
+	SetTile $0422, $01
+	SetTile $0423, $01
+	SetTile $0424, $01
+	SetTile $0425, $01
+	SetTile $0426, $01
+	SetTile $0427, $01
+	SetTile $0428, $01
+	SetTile $0429, $01
+	SetTile $042A, $01
+	SetTile $042B, $01
+	SetTile $042C, $01
+	SetTile $042D, $01
+	SetTile $042E, $01
+	SetTile $042F, $01
+	
+	SetTile $0430, $01
+	SetTile $0431, $01
+	SetTile $0432, $01
+	SetTile $0433, $01
+	SetTile $0434, $01
+	SetTile $0435, $01
+	SetTile $0436, $01
+	SetTile $0437, $01
+	SetTile $0438, $01
+	SetTile $0439, $01
+	SetTile $043A, $01
+	SetTile $043B, $01
+	SetTile $043C, $01
+	SetTile $043D, $01
+	SetTile $043E, $01
+	SetTile $043F, $01
 	
 	jsr SetupVideo				; Setup video modes and other stuff, then turn on screen
 	
